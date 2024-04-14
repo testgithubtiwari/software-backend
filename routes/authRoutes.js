f4236cb68c274c24cbf65b372b9b8eb2e9469a0e
testgithubtiwari <rt936649@gmail.com>
2024-04-15 00:42:40 +0530

    new changes done in swagger

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -54,7 +54,64 @@ const router = express.Router();
  */
 router.post("/register", createUser);
 
+/**
+ * @swagger
+ * /software-project.in/user/login:
+ *   post:
+ *     summary: Login User
+ *     description: Endpoint to login user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Login successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       '400':
+ *         description: Please provide all the required fields
+ *       '401':
+ *         description: Email Id is not regsitered or password is incorrect
+ *       '500':
+ *         description: Internal server error
+ */
 router.post("/login", LoginUser);
+
+/**
+ * @swagger
+ * /software-project.in/user/allUsers:
+ *   get:
+ *     summary: Get All Users
+ *     description: Endpoint to get all users
+ *     responses:
+ *       '200':
+ *         description: All users fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+
 router.get("/allUsers", getAllUsers);
 router.get("/user", getUser);
 router.post("/isProfileCompleted", isProfileCompleted);
